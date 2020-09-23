1de35378ba01a23d445e6c03d6d256c892d10621
Lyubomir Dias <lyubomirdias@gmail.com>
2020-09-23 20:19:09 +0100

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
 
+def default_url_options
+  { host: ENV["DOMAIN"] || "www.bisimpact.com" }
+end
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,11 +29,13 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import '../custom/navbar.js'
+import { initMapbox } from '../plugins/init_mapbox';
 
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
 // need to pseed down the video banner
 
