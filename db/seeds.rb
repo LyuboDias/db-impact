a407f43465ac5cde74e15494b13f35e22de50c59
Lyubomir Dias <lyubomirdias@gmail.com>
2020-09-30 22:03:07 +0100

    added new navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+lu = Staff.create(f_name: 'Lyubomir', l_name: 'Dias', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'lyubomirdias@gmail.com', phone: '0757635246')
+
+deep = Staff.create(f_name: 'Deep', l_name: 'Biswas', job_title: 'CEO', bio: 'Born to immigrants, I am a Bilingual individual in English and Bengali who is a British National, and possesses an Indian Overseas Citizenship (status allowing me to work and live in India visa-free). As well as holding a BA in Philosophy, Politics and Economics and an MPhil in Development Studies, one which has qualified me to continue as a PhD/DPhil candidate (at the University of Cambridge & elsewhere), I have accumulated policy experience with political parties, think tanks and prominent government departments. I spent 8 months working as an Economist at the UK Foreign and Commonwealth Office before travelling to China. In China, I encouraged students to apply to top UK institutions. In the private sector, I spent 3 months working for the leading international development strategy consultancy - Dalberg in Geneva, Switzerland working on UNHCR, UICC and DFID projects. I now work as an Economist with the Department for International Development (DFID) and country-experience includes Uganda, Nigeria, Tunisia, Sudan and as a Diplomat in Tanzania. In the summer of 2020, I learnt to code and build simple web applications.', email: 'deepbiswas@hotmail.com', phone: '0783635246')
+
+lu = Staff.create(f_name: 'Random', l_name: 'Person', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'randomp@gmail.com', phone: '0757632836')
+
+lu = Staff.create(f_name: 'Bob', l_name: 'Sponge', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'spongebob@gmail.com', phone: '0757635246')
+
+
