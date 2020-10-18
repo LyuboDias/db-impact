722364a1638e9549029cf786045ffb419b3d2a64
Deep Biswas <deepbiswas@hotmail.com>
2020-10-18 12:07:47 +0100

    Istvan's changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 
+puts 'cleaning database'
+Staff.destroy_all
+puts 'Database cleaned.'
+puts 'Creating new staff member.'
 
-lu = Staff.create(f_name: 'Lyubomir', l_name: 'Dias', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'lyubomirdias@gmail.com', phone: '0757635246')
+lu = Staff.new({ 
+  f_name: 'Lyubomir', 
+  l_name: 'Dias', 
+  job_title: 'Software Engineer/Tech Lead', 
+  bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', 
+  email: 'lyubomirdias@gmail.com', 
+  phone: '0757635246'
+})
+lu_pic = URI.open("https://res.cloudinary.com/dhoxwyrvn/image/upload/v1603018872/Nigella_Lawson_2_xzjnzr.jpg")
+lu.image.attach(io: lu_pic, filename: "lu_pic.jpg", content_type: "image/jpg")
+lu.save!
 
-deep = Staff.create(f_name: 'Deep', l_name: 'Biswas', job_title: 'CEO', bio: 'Debdipto (Deep) Biswas was born in London to immigrant parents, and grew up as a third-culture kid speaking Bengali at home.
+deep = Staff.new({
+  f_name: 'Deep', 
+  l_name: 'Biswas', 
+  job_title: 'CEO', 
+  bio: 'Debdipto (Deep) Biswas was born in London to immigrant parents, and grew up as a third-culture kid speaking Bengali at home.
 
-He has worked in a series of international development/policy roles in both the public and private sectors for organisations such as the Foreign and Commonwealth Office (FCO), Dalberg Global Development Advisors and the Department for International Development (DFID). His experience covers the UK, Switzerland, Uganda, Nigeria, Tunisia, Sudan and as a diplomat in Tanzania, where he was the youngest in the whole High Commission.
+  He has worked in a series of international development/policy roles in both the public and private sectors for organisations such as the Foreign and Commonwealth Office (FCO), Dalberg Global Development Advisors and the Department for International Development (DFID). His experience covers the UK, Switzerland, Uganda, Nigeria, Tunisia, Sudan and as a diplomat in Tanzania, where he was the youngest in the whole High Commission.
 
-He has worked on topics such as International Trade, International Finance Institutions, Sustainble Private Sector Growth and Agri-Business.
+  He has worked on topics such as International Trade, International Finance Institutions, Sustainble Private Sector Growth and Agri-Business.
 
-He holds a BA (Hons) Philosophy, Politics and Economics (PPE) from the University of Warwick and an MPhil in Development Studies from the University of Cambridge, Jesus College, where he achieved the PhD threshold in all his modules.', email: 'deepbiswas@hotmail.com', phone: '0783635246')
+  He holds a BA (Hons) Philosophy, Politics and Economics (PPE) from the University of Warwick and an MPhil in Development Studies from the University of Cambridge, Jesus College, where he achieved the PhD threshold in all his modules.', 
+  email: 'deepbiswas@hotmail.com', 
+  phone: '0783635246'
+})
+deep_pic = URI.open("https://res.cloudinary.com/dhoxwyrvn/image/upload/v1598125079/tq6rsvgiyh5m3cpc99ui6o2dyh4h.jpg")
+deep.image.attach(io: deep_pic, filename: "deep_pic.jpg", content_type: "image/jpg")
+deep.save!
 
-lu = Staff.create(f_name: 'Random', l_name: 'Person', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'randomp@gmail.com', phone: '0757632836')
+# lu = Staff.create(f_name: 'Random', l_name: 'Person', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'randomp@gmail.com', phone: '0757632836')
 
-lu = Staff.create(f_name: 'Bob', l_name: 'Sponge', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'spongebob@gmail.com', phone: '0757635246')
+# lu = Staff.create(f_name: 'Bob', l_name: 'Sponge', job_title: 'Software Engineer/Tech Lead', bio: 'Ensured high availability of the Asset Arena Transfer Agency product via coordinated team work between domestic & offshore resources. Provide technical support to team members and members of other departments integral to the successful completion of modules.', email: 'spongebob@gmail.com', phone: '0757635246')
 
-
+puts "seeding is finished"
