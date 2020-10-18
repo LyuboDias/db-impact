7c5ac41fb1e7ae3b05081145e2477763a1fa57ea
Lyubomir Dias <lyubomirdias@gmail.com>
2020-10-18 22:20:08 +0100

    added blog pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :staff, only: [:index, :show, :new, :create]
+  resources :blogs, only: [:index, :show, :new, :create]
   get '/videos' => 'pages#videos'
 end
