6ac2846dd2c0d7e1dbf90bc739f1b63c2ac596dc
Deep Biswas <deepbiswas@hotmail.com>
2020-11-14 14:38:14 +0000

    for Github

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_142719) do
+ActiveRecord::Schema.define(version: 2020_11_14_134419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_11_12_142719) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
     t.string "link"
+    t.text "briefdescription"
   end
 
   create_table "users", force: :cascade do |t|
